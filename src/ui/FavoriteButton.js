c090084ae80ba959c45fce1ef7eeedfb046180eb
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-04-13 12:48:32 +0200

    conflict

--- src/ui/FavoriteButton.js
+++ src/ui/FavoriteButton.js
@@ -112,7 +112,6 @@ FavoriteButton = function(c) {
                         text: getTitle(i18n.share),
                         iconCls: 'ns-menu-item-favorite-share',
                         disabled: function() {
-<<<<<<< HEAD
                             var fav = instanceManager.getStateFavorite();
 
                             if (fav && (!fav.getAccess() || fav.getAccess().manage)) {
@@ -121,16 +120,6 @@ FavoriteButton = function(c) {
 
                             return true;
                         }(),
-=======
-                            var fav = instanceManager.getStateFavorite();
-
-                            if (fav && (!fav.getAccess() || fav.getAccess().manage)) {
-                                return false;
-                            }
-
-                            return true;
-                        }(),
->>>>>>> 585644b7e5674836128a7f03cef26644c1840b0f
                         handler: function() {
                             instanceManager.getSharingById(instanceManager.getStateFavoriteId(), function(r) {
                                 SharingWindow(c, r).show();
