682fbf32bd7f44de19e398309b3de5fa5634d09f
Ilya Nee <neeilya@mail.ru>
2017-01-25 13:46:39 +0100

    Added option for colorizing each cell by its own legendset according to its indicator

--- src/pivot/Table.js
+++ src/pivot/Table.js
@@ -86,7 +86,7 @@ Table = function(layout, response, colAxis, rowAxis, options) {
 
     getTdHtml = function(config, metaDataId) {
         var bgColor,
-            legends,
+            legends = [],
             colSpan,
             rowSpan,
             htmlValue,
@@ -135,23 +135,22 @@ Table = function(layout, response, colAxis, rowAxis, options) {
         tdCount = tdCount + 1;
 
         if (isValue) {
-            if (legendSet) {
-                legends = legendSet.legends;
+            var value = parseFloat(config.value);
+
+            if (legendSetByDataItem) {
+                if (config.dxId && response.metaData.items[config.dxId].legendSet) {
+                    var legendSetId = response.metaData.items[config.dxId].legendSet,
+                        _legendSet = appManager.getLegendSetById(legendSetId);
+
+                    legends = _legendSet.legends;
+                }
+            } else {
+                legends = legendSet ? legendSet.legends : [];
             }
 
-            if (legendSetByDataItem && response.metaData.items[config.dxId].legendSet) {
-                var legendSetId = response.metaData.items[config.dxId].legendSet,
-                    _legendSet = appManager.getLegendSetById(legendSetId),
-                    value = parseFloat(config.value);
-
-                legends = _legendSet.legends;
-            }
-
-            if (legendSet || (legendSetByDataItem && response.metaData.items[config.dxId].legendSet)) {
-                for (var i = 0; i < legends.length; i++) {
-                    if (numberConstrain(value, legends[i].startValue, legends[i].endValue) === value) {
-                        bgColor = legends[i].color;
-                    }
+            for (var i = 0; i < legends.length; i++) {
+                if (numberConstrain(value, legends[i].startValue, legends[i].endValue) === value) {
+                    bgColor = legends[i].color;
                 }
             }
         }
@@ -180,19 +179,6 @@ Table = function(layout, response, colAxis, rowAxis, options) {
         html += '<td ' + (config.uuid ? ('id="' + config.uuid + '" ') : '');
         html += ' class="' + cls + '" ' + colSpan + rowSpan;
         html += config.title ? ' title="' + config.title + '" ' : '';
-
-        //if (bgColor && isValue) {
-            //html += 'style="color:' + bgColor + ';padding:' + displayDensity + '; font-size:' + fontSize + ';"' + '>' + htmlValue + '</td>';
-            //html += '>';
-            //html += '<div class="legendCt">';
-            //html += '<div class="number ' + config.cls + '" style="padding:' + displayDensity + '; padding-right:3px; font-size:' + fontSize + '">' + htmlValue + '</div>';
-            //html += '<div class="arrowCt ' + config.cls + '">';
-            //html += '<div class="arrow" style="border-bottom:8px solid transparent; border-right:8px solid ' + bgColor + '">&nbsp;</div>';
-            //html += '</div></div></div></td>';
-        //}
-        //else {
-        //    html += 'style="' + (bgColor && isValue ? 'color:' + bgColor + '; ' : '') + '">' + htmlValue + '</td>';
-        //}
 
         if (legendDisplayStyle === optionConfig.getLegendDisplayStyle('fill').id) {
             if(bgColor) {
@@ -520,7 +506,7 @@ Table = function(layout, response, colAxis, rowAxis, options) {
                     htmlValue: htmlValue,
                     empty: empty,
                     uuids: uuids,
-                    dxId: rric.getDxIdByIds(response.metaData.dx)
+                    dxId: rric.getDxIdByIds(response.metaData.dimensions.dx)
                 });
 
                 // map element id to dim element ids
