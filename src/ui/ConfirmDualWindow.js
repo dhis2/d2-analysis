93d6a1f4e578dd06e5ca7b43860f3f54e0a76267
Jan Henrik Overland <janhenrik.overland@gmail.com>
2017-05-22 23:44:26 +0200

    dual window

--- src/ui/ConfirmDualWindow.js
+++ src/ui/ConfirmDualWindow.js
@@ -3,3 +3,49 @@ import { ConfirmButton } from '../ui/ConfirmButton';
 export var ConfirmDualWindow;
 
 ConfirmDualWindow = function(refs, title, msg, btnText, fn, applyConfig) {
+
+
+
+
+    var window = Ext.create('Ext.window.Window', Object.assign({
+        bodyStyle: 'background:#fff; padding:30px 60px 26px 42px',
+        defaults: defaults,
+        modal: true,
+        preventHeader: true,
+        resizable: false,
+        closeable: false,
+        items: [
+            {
+                html: title,
+                bodyStyle: 'padding:0; border:0 none; font-size:16px',
+                style: 'margin-bottom:20px'
+            },
+            {
+                html: msg,
+                bodyStyle: 'padding:0; border:0 none; font-size:13px',
+                style: 'margin-bottom:16px'
+            },
+            {
+                layout: 'hbox',
+                defaults: defaults,
+                items: [
+                    confirmButton,
+                    {
+                        width: 10
+                    },
+                    cancelButton
+                ]
+            }
+        ],
+        listeners: {
+            afterrender: function() {
+                this.setPosition(this.getPosition()[0], this.getPosition()[1] / 2);
+            },
+            show: function() {
+                confirmButton.focus(false, 50);
+            }
+        }
+    }, applyConfig));
+
+    return window;
+};
