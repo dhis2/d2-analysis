effd98b4719bb09f5e91403a9b0750bd21ec9639
Adrian Quintana <adrianquintana@gmail.com>
2018-02-01 10:11:56 +0000

    extract redundant code

--- src/ui/MentionToolbar.js
+++ src/ui/MentionToolbar.js
@@ -8,6 +8,28 @@ MentionToolbar = function (refs) {
     
     var i18n = i18nManager.get();
 
+
+    var createMentionLabelsForUser = function(users, currentMention){
+        return users
+                .filter(user => user.userCredentials.username.includes(currentMention))
+                .map((user) => {
+                    return {
+                        xtype: 'label',
+                        html:  user.displayName + " (" + user.userCredentials.username + ")",
+                        listeners: {
+                            'render': function(label) {
+                                label.getEl().on('click', function() {
+                                    splitText.splice(-1,1);
+                                    var newText = splitText.join("@") + "@" + user.userCredentials.username;
+                                    component.setValue(newText);
+                                    mentionsPanel.hide();
+                                }, label);
+                            }
+                        }
+                    }
+                });
+    }
+
     var mentionsPanel = Ext.create('Ext.panel.Panel', {
         floating: true,
         layout: {
@@ -27,45 +49,10 @@ MentionToolbar = function (refs) {
     
                 mentionsPanel.removeAll(true);
     
-                var potentialMostMentionedUsers= appManager.mostMentionedUsers
-                    .filter(user => user.userCredentials.username.includes(currentMention))
-                    .map((user) => {
-                        return {
-                            xtype: 'label',
-                            html:  user.displayName + " (" + user.userCredentials.username + ")",
-                            listeners: {
-                                'render': function(label) {
-                                    label.getEl().on('click', function() {
-                                        splitText.splice(-1,1);
-                                        var newText = splitText.join("@") + "@" + user.userCredentials.username;
-                                        component.setValue(newText);
-                                        mentionsPanel.hide();
-                                    }, label);
-                                }
-                            }
-                        }
-                    });
+                var potentialMostMentionedUsers= createMentionLabelsForUser(appManager.mostMentionedUsers, currentMention);
+                var potentialUsers = createMentionLabelsForUser(appManager.users, currentMention);
     
-                var potentialUsers = appManager.users
-                    .filter(user => user.userCredentials.username.includes(currentMention))
-                    .map((user) => {
-                        return {
-                            xtype: 'label',
-                            html: user.displayName + " (" + user.userCredentials.username + ")",
-                            listeners: {
-                                'render': function(label) {
-                                    label.getEl().on('click', function() {
-                                        splitText.splice(-1,1);
-                                        var newText = splitText.join("@") + "@" + user.userCredentials.username;
-                                        component.setValue(newText);
-                                        mentionsPanel.hide();
-                                    }, label);
-                                }
-                            }
-                        }
-                    });
-    
-                if (potentialMostMentionedUsers != null && potentialMostMentionedUsers.length > 0){
+                if (potentialMostMentionedUsers && potentialMostMentionedUsers.length > 0){
                     mentionsPanel.add({
                         html: i18n.most_common_users_matching + ' @' + currentMention,
                         cls: 'mentionsTitle',
@@ -73,7 +60,7 @@ MentionToolbar = function (refs) {
                     mentionsPanel.add(potentialMostMentionedUsers);
                     
                 }
-                if (potentialUsers != null && potentialUsers.length > 0){
+                if (potentialUsers && potentialUsers.length > 0){
                     mentionsPanel.add({
                         html: i18n.other_users_matching + ' @' + currentMention,
                         cls: 'mentionsTitle',
@@ -81,7 +68,7 @@ MentionToolbar = function (refs) {
                     mentionsPanel.add(potentialUsers);
                 }
     
-                if (potentialMostMentionedUsers != null && potentialMostMentionedUsers.length == 0 && potentialUsers != null && potentialUsers.length == 0){
+                if (potentialMostMentionedUsers && potentialMostMentionedUsers.length == 0 && potentialUsers && potentialUsers.length == 0){
                     mentionsPanel.hide();
                 }
                 else{
