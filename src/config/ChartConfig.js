891c4de5f5bf5c55e2c14c7d15e2fddf55b46b86
Edoardo Sabadelli <edoardo@dhis2.org>
2018-11-28 14:59:12 +0100

    Run prettier, ignore

--- src/config/ChartConfig.js
+++ src/config/ChartConfig.js
@@ -15,7 +15,7 @@ ChartConfig = function() {
         area: 'area',
         pie: 'pie',
         radar: 'radar',
-        gauge: 'gauge'
+        gauge: 'gauge',
     };
 
     t.server = {
@@ -27,38 +27,51 @@ ChartConfig = function() {
         area: 'AREA',
         pie: 'PIE',
         radar: 'RADAR',
-        gauge: 'GAUGE'
+        gauge: 'GAUGE',
     };
 
     t.consts = {
         domain: 'domain_',
         targetLine: 'targetline_',
         baseLine: 'baseline_',
-        trendLine: 'trendline_'
+        trendLine: 'trendline_',
     };
 
     t.style = {
         inset: 30,
-        fontFamily: 'Arial,Sans-serif,Roboto,Helvetica,Consolas'
+        fontFamily: 'Arial,Sans-serif,Roboto,Helvetica,Consolas',
     };
 
     t.theme = {
-        dv1: ['#94ae0a', '#1d5991', '#a61120', '#ff8809', '#7c7474', '#a61187', '#ffd13e', '#24ad9a', '#a66111', '#414141', '#4500c4', '#1d5700']
+        dv1: [
+            '#94ae0a',
+            '#1d5991',
+            '#a61120',
+            '#ff8809',
+            '#7c7474',
+            '#a61187',
+            '#ffd13e',
+            '#24ad9a',
+            '#a66111',
+            '#414141',
+            '#4500c4',
+            '#1d5700',
+        ],
     };
 
     // transient
 
-    t.c2s = {};
-    t.s2c = {};
+    t.c2s = {};
+    t.s2c = {};
 
     // initialize
-    (function() {
-        Object.keys(t.client).forEach(function(key) {
+    (function() {
+        Object.keys(t.client).forEach(function(key) {
             t.c2s[t.client[key]] = t.server[key];
         });
 
-        Object.keys(t.server).forEach(function(key) {
+        Object.keys(t.server).forEach(function(key) {
             t.s2c[t.server[key]] = t.client[key];
         });
-    }());
+    })();
 };
