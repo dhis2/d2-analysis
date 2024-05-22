4f77d58e5f8db83604c7f40f65dd234dbf3dde9e
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2024-05-22 10:45:11 +0200

    chore: stringify input

--- src/util/toPatchData.js
+++ src/util/toPatchData.js
@@ -2,7 +2,7 @@ import isObject from 'd2-utilizr/lib/isObject';
 
 export const toPatchData = properties => {
     if (!isObject(properties)) {
-        throw `toPatchData: not an object: ${properties}`
+        throw `toPatchData: not an object: ${JSON.stringify(properties)}`
     }
 
     return Object.entries(properties).reduce((arr, [key, value]) => {
